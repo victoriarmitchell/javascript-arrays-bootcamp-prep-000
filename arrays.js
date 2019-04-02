9843c6047668d13c216bf4a1616184e44f72e923
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-02 03:24:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,11 @@
-var chocolateBars = ['snickers','hundred grand', 'kitkat','skittles']
+var chocolateBars = ['snickers','hundred grand', 'kitkat','skittles'];
 
-function addElementToBeginningOfArray() {
-  
+function addElementToBeginningOfArray(array,element) {
+  newArray = [element,...array];
+  return newArray;
 }
 
-
-function destructivelyAddElementToBeginningOfArray
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element);
+  return array;
+}
