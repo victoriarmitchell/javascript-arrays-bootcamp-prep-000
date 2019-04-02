76f6555a76e128270de50bfa2ba68ea2b28ad90b
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-02 03:28:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,8 @@ function destructivelyAddElementToEndOfArray(array,element) {
   array.push(element);
   return array;
 }
+
+function accessElementInArray(array,index) {
+  return array[index];
+}
+
